d10a90bccda7a2b8142316abda51e9e53c1ddfe5
Eduardo Oliveira <dudu_olive43@hotmail.com>
2025-06-04 12:57:22 -0300

    primeiros programas

--- main.py
+++ main.py
@@ -1,2 +1,6 @@
 from src.BancoDados.dbConfig import conectar_bd, consultar_dados, executar_query
+from src.Classes import consultaPaciente, exame, paciente
 
+teste = {"nome":"teste", "idade":22, "cpf":"123456", "rg":"2323232", "cep":"030303", "endereco":"av. teste", "complemento":"tes22", "num_casa":"20A", "email":"teste@teste.com", "nome_mae":"teste mae"}
+
+paciente.cadastrar_paciente(teste)
