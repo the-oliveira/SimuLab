b33bb052a8e98e99fa7b465f484aff8489377126
Eduardo Oliveira <dudu_olive43@hotmail.com>
2025-07-03 12:18:46 -0300

    Update run.py

--- src/Web/run.py
+++ src/Web/run.py
@@ -4,13 +4,3 @@ from Classes import cad_exames, cad_paciente, consulta_exame, consulta_pac
 from utilidades import validadores
 
 
-app = Flask(__name__)
-
-@app.route('/')
-
-def index():
-    #GET com a pagina inicial do projeto
-    return render_template('\template\index.html')
-
-
-@app.route('/pacientes/novo', methods=['GET', 'POST'])
